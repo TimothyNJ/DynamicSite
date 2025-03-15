58615d99c9701ae2ba9fa9308c62583a6b6881ce
TimothyNJ <timothynicholasjones@gmail.com>
2025-03-15 16:48:20 -0700

    updated main.js to load home page

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ import { initRouter } from "./navigation/router.js";
 import { initBuffers } from "./layout/buffers.js";
 import { initDimensions } from "./layout/dimensions.js";
 import { initNavbar } from "./navigation/navbar.js";
+import { loadPage } from "./navigation/router.js"; // Ensure loadPage is imported here
 
 // Initialize all modules when the DOM is loaded
 document.addEventListener("DOMContentLoaded", () => {
@@ -10,4 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
   initBuffers();
   initDimensions();
   initNavbar();
+
+  // Explicitly load the home page by default on initial page load
+  loadPage("pages/home.html");
 });
