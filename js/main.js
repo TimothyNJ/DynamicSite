a160057a6088cd9dfdedc1d701c6c611dc756832
TimothyNJ <timothynicholasjones@gmail.com>
2025-03-15 20:12:03 -0700

    updates to main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 // js/main.js
 import { initRouter } from "./navigation/router.js";
 import { initBuffers } from "./layout/buffers.js";
-import { initDimensions } from "./layout/dimensions.js";
-import { initializeNavbar } from "./navigation/navbar.js"; // Changed function name
+import { updateDimensions } from "./layout/dimensions.js"; // Changed from initDimensions to updateDimensions
+import { initializeNavbar } from "./navigation/navbar.js";
 
 // Initialize all modules when the DOM is loaded
 document.addEventListener("DOMContentLoaded", () => {
   initRouter(); // This already loads home page
   initBuffers();
-  initDimensions();
-  initializeNavbar(); // Changed function name
+  updateDimensions(); // Changed from initDimensions to updateDimensions
+  initializeNavbar();
 });
