c9bad64f045bbb078f7efc01a42e1ac33ec6f510
TimothyNJ <timothynicholasjones@gmail.com>
2025-04-24 21:51:00 -0700

    Revert "remove labels"
    This reverts commit 906443dc135e31792ec93ea9f2612b67704644a2.

--- js/selectors/selector-manager.js
+++ js/selectors/selector-manager.js
@@ -92,9 +92,10 @@ window.SelectorManager = (function() {
         // Create temporary instance to generate HTML
         const temp = new window.GenericSelector(config.selector, config.name, config);
         
-        // Add section HTML - without heading labels
+        // Add section HTML
         html += `
           <div class="settings-section">
+            <h3>${config.name.charAt(0).toUpperCase() + config.name.slice(1)}</h3>
             <div class="slider-container ${key}-container">
               ${temp.generateHTML()}
             </div>
